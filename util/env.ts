0acf10b1b731f207f373d3fc63fa5cc11e5ece19
Jamalam360 <56727311+Jamalam360@users.noreply.github.com>
2022-03-05 09:14:22 +0000

    fix: don't look for .env on Deploy

--- util/env.ts
+++ util/env.ts
@@ -1,11 +1,13 @@
 import { parse } from "https://deno.land/x/dot_env@0.2.0/mod.ts";
 
 export async function populate() {
-  const env = parse(await Deno.readTextFile(".env"));
-  for (const key in env) {
-    if (Object.prototype.hasOwnProperty.call(env, key)) {
-      const element = env[key];
-      Deno.env.set(key, element);
+  if (Deno.env.get("DENO_DEPLOYMENT_ID") == undefined) {
+    const env = parse(await Deno.readTextFile(".env"));
+    for (const key in env) {
+      if (Object.prototype.hasOwnProperty.call(env, key)) {
+        const element = env[key];
+        Deno.env.set(key, element);
+      }
     }
   }
 }
